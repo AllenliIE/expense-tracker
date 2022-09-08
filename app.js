e0815bfa621980e34f84900c9d7487e22a0d7fd9
AllenliIE <allen.li.service@gmail.com>
2022-09-08 16:12:09 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//Setting Mongoose
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+//Connection mongoose
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('Mongodb error!')
+})
+db.once('open', () => {
+  console.log('Mongodb connected!')
+})
+
 
 //Setting port 3000
 app.listen(port, () => {
