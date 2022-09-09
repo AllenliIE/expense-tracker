83033c13d97da5917cad61e44d76c44bdb24d50f
AllenliIE <allen.li.service@gmail.com>
2022-09-09 14:40:37 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const Record = require('./models/record')
-//Setting body-[arser
+//Setting body-parser
 const bodyParser = require('body-parser')
 //Setting method-override
 const methodOverride = require('method-override')
+//Setting routes
+const routes = require('./routes')
 //Use handlebars to template engine setting
 const exphbs = require('express-handlebars')
 //Setting exphbs and parameters
@@ -29,57 +30,7 @@ db.once('open', () => {
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-//Setting record to render
-app.get('/', (req, res) => {
-  Record.find()
-    .lean()
-    .then(records => res.render('index', { records }))
-    .catch(error => console.log(error))
-})
-
-app.get('/records/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/records', (req, res) => {
-  const { name, date, amount } = req.body
-
-  return Record.create({ name, date, amount })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/records/:id/edit', (req, res) => {
-  const id = req.params.id
-
-  return Record.findById(id)
-    .then((record) => res.render('edit', { record }))
-    .catch(error => console.log(error))
-})
-
-app.put('/records/:id', (req, res) => {
-  const id = req.params.id
-  const { name, date, amount } = req.body
-
-  return Record.findById(id)
-    .then(record => {
-      record.name = name
-      record.date = date
-      record.amount = amount
-      return record.save()
-    })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.delete('/records/:id', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .then(record => record.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 //Setting port 3000
 app.listen(port, () => {
